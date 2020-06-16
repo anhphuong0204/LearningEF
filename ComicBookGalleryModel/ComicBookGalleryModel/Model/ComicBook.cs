688019c24d0032a0926bf8559e486c0ba678cbd8
anhphuong0204 <phuong.nguyen02042000@hcmut.edu.vn>
2020-06-17 00:13:05 +0700

    Change seri foreign key inside comic model

--- ComicBookGalleryModel/ComicBookGalleryModel/Model/ComicBook.cs
+++ ComicBookGalleryModel/ComicBookGalleryModel/Model/ComicBook.cs
@@ -9,11 +9,13 @@ namespace ComicBookGalleryModel.Model
     public class ComicBook
     {
         public int Id { get; set; }
-        public Series Series { get; set; }
+        public int SeriesId { get; set; }
         public int IssueNumber { get; set; }
         public string Description { get; set; }
         public DateTime PublishedOn { get; set; }
         public decimal? AverageRating { get; set; }
+
+        public Series Series { get; set; }
 
         public string DisplayText {
             get
